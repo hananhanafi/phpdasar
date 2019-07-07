0ed9af23bd2f5423a55d741c5e105bbbc25455a2
Hanafih <hananhanafi@mail.ugm.ac.id>
2019-07-08 00:54:10 +0700

    Login

---
+++ pertemuan15/login.php
@@ -0,0 +1,58 @@
+<?php
+    require 'functions.php';
+
+    if(isset($_POST["login"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $cekuser = mysqli_query($conn, "SELECT * FROM users WHERE username ='$username'");
+
+        if(mysqli_num_rows($cekuser) ===1){
+            $row = mysqli_fetch_assoc($cekuser);
+            
+            if(password_verify($password, $row["password"])){
+                header("Location: index.php");
+                exit;
+            }
+        }
+        $error = true;
+
+
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    
+    <?php if(isset($error)) : ?>
+        <p style="color:red;">Username/Password salah!</p>
+    <?php endif;?>
+
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="password">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+        
+    </form>
+</body>
+</html>
