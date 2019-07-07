db40496bedbcc94b62fb145c23adbc7d4e456a11
Hanafih <hananhanafi@mail.ugm.ac.id>
2019-07-07 23:54:24 +0700

    Registrasi

---
+++ pertemuan14/index.php
@@ -0,0 +1,61 @@
+<?php
+//koneksi ke database
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+if(isset($_POST["cari"])){
+    $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br>
+    <br>
+    <form action="" method="post">
+        <input type="text" id="keyword" name="keyword" size="50" autofocus placeholder="Masukan keyword pencarian..." autocomplete="off">
+        <button type="submit" name="cari">Cari</button>
+    </form>
+    <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            
+        </tr>
+        <?php $i = 1;?>
+        <?php foreach($mahasiswa as $mhs) : ?>
+        <tr>
+            
+            <td><?= $i?></td>
+            <td><a href="ubah.php?id=<?= $mhs["id"]?>">Ubah</a> | 
+            <a href="hapus.php?id=<?= $mhs["id"];?>" onclick="return confirm('yakin?');">hapus</a></td>
+            <td><img src="img/<?= $mhs["gambar"]?>" alt="" width="50"></td>
+            <td><?= $mhs["nrp"];?></td>
+            <td><?= $mhs["nama"];?></td>
+            <td><?= $mhs["email"];?></td>
+            <td><?= $mhs["jurusan"];?></td>
+        </tr>
+        <?php $i++;?>
+        <?php endforeach;?>
+
+    </table>
+</body>
+</html>
---
+++ pertemuan14/tambah.php
@@ -0,0 +1,64 @@
+<?php
+    require 'functions.php';
+
+    if(isset($_POST["submit"])){
+        //cek apakah data berhasil ditambahkan atau tidak
+
+        if(tambah($_POST) > 0){
+            echo "<script>
+                alert('Data berhasil ditambahkan');
+                document.location.href = 'index.php';
+                </script>
+            ";
+        }else{
+            echo "<script>
+                alert('Data gagal ditambahkan');
+                document.location.href = 'tambah.php';
+                </script>
+            ";
+        }
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1> 
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>   
+            <li>
+                <label for="nrp">NIM :</label>
+                <input type="text" id="nrp" name="nrp">
+            </li>
+            <li>
+                <label for="nama">Nama :</label>
+                <input type="text" id="nama" name="nama">
+            </li>
+            <li>
+                <label for="email">Email :</label>
+                <input type="text" id="email" name="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan :</label>
+                <input type="text" id="jurusan" name="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label>
+                <input type="file" id="gambar" name="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah</button>
+            </li>
+        </ul>
+        
+    </form>
+</body>
+</html>
